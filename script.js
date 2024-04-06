8567e0a6fb69d71799b175121b8d16da4bc08b61
Dev Aggarwal <devaggarwal1009@gmail.com>
2024-04-07 01:49:50 +0530

    score, start and pause funtionality added

--- script.js
+++ script.js
@@ -1,6 +1,86 @@
 const canvas = document.querySelector("canvas");
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
+const start = document.getElementById("start");
+const restart = document.getElementById("restart");
+const players = document.getElementsByClassName("players");
+const player1 = document.getElementById("player1");
+const player2 = document.getElementById("player2");
+let player1score = 0;
+let player2score = 0;
+let play = false;
+let pause = false;
+
+restart.addEventListener("click", (e) => {
+  initBall();
+  restart.style.display = "none";
+  play = true;
+  animate();
+});
+
+window.addEventListener("keydown", (e) => {
+  if (e.key == "Enter" && !play) {
+    player1.style.display = "block";
+    player2.style.display = "block";
+    start.style.display = "none";
+    play = true;
+    pause = false;
+    initBall();
+    init();
+    animate();
+
+    if(restart.style.display == 'inline'){
+      restart.style.display = 'none';
+    }
+
+  }
+
+  if(e.key == 'p'){
+    if(!pause){
+      pause = true;
+    }
+    else{
+      pause = false;
+      animate()
+    }
+  }
+
+  // if(e.key = 'r' && play){
+  //   start.style.display = "inline";
+  //   player1score = 0;
+  //   player1score = 0;
+  //   player1.textContent = 0;
+  //   player2.textContent = 0;
+  //   player1.style.display = "none";
+  //   player2.style.display = "none";
+  //   play = false;
+  //   ball = null;
+  //   recLeft = null;
+  //   rightRec = null;
+  //   window.cancelAnimationFrame(myRequestId)
+
+  //   // playStart()
+  //   animate()
+  // }
+});
+
+let playStart;
+
+start.addEventListener("click", playStart = () => {
+  player1.style.display = "block";
+  player2.style.display = "block";
+  start.style.display = "none";
+  play = true;
+  initBall();
+  init();
+  animate();
+});
+
+window.addEventListener("resize", (e) => {
+  canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
+  
+});
 
 const c = canvas.getContext("2d");
 
@@ -13,19 +93,20 @@ function Rectangle(x, y, width, height) {
   this.draw = () => {
     c.beginPath();
     c.fillStyle = "brown";
-    c.fillRect(this.x, this.y, this.width, this.height);
+    c.strokeStyle = "black";
+    c.rect(this.x, this.y, this.width, this.height);
+    c.lineWidth = 3;
     c.stroke();
+    c.fill();
+    // c.fillRect(this.x, this.y, this.width, this.height);
   };
 
-  this.update = (coun) => {
+  this.update = () => {
     this.draw();
   };
 }
 
-const constDis = 15;
-
-// const recLeft = new Rectangle(80, (innerHeight-100)/2, 20, 100);
-// recLeft.draw()
+const constDis = 25;
 
 let rectancles = [];
 
@@ -37,28 +118,30 @@ let posRightY = (innerHeight - recHeight) / 2;
 let recRight;
 
 document.addEventListener("keydown", (e) => {
-  if (e.key == "ArrowDown") {
-    if (posRightY + recHeight < innerHeight) {
-      posRightY += constDis;
-      init();
+  if (play) {
+    if (e.key == "ArrowDown") {
+      if (posRightY + recHeight < innerHeight) {
+        posRightY += constDis;
+        init();
+      }
     }
-  }
-  if (e.key == "ArrowUp") {
-    if (posRightY > 0) {
-      posRightY -= constDis;
-      init();
+    if (e.key == "ArrowUp") {
+      if (posRightY > 0) {
+        posRightY -= constDis;
+        init();
+      }
     }
-  }
-  if (e.key == "s") {
-    if (posLeftY + recHeight < innerHeight) {
-      posLeftY += constDis;
-      init();
+    if (e.key == "s") {
+      if (posLeftY + recHeight < innerHeight) {
+        posLeftY += constDis;
+        init();
+      }
     }
-  }
-  if (e.key == "w") {
-    if (posLeftY > 0) {
-      posLeftY -= constDis;
-      init();
+    if (e.key == "w") {
+      if (posLeftY > 0) {
+        posLeftY -= constDis;
+        init();
+      }
     }
   }
 });
@@ -84,35 +167,55 @@ function Circle(x, y, dx, dy, radius) {
     c.fill();
   };
 
+
   this.update = (leftRec, rightRec) => {
-    if (this.x - this.radius < 0 || this.x + this.radius > innerWidth) {
-      this.dx = -this.dx;
-    }
+    //logic for bouncing it back if the ball hits the top and bottom edge
     if (this.y - this.radius < 0 || this.y + this.radius > innerHeight) {
       this.dy = -this.dy;
     }
-    // bouncing the ball if it hits the rectangle
-    
-    if((this.y > leftRec.y && this.y < leftRec.y + leftRec.height) && (this.x - this.radius <= leftRec.x + leftRec.width) && (this.dx <= 0)){
+
+    //logic for what happens if the ball hits a rectancle
+    if (
+      this.y > leftRec.y &&
+      this.y < leftRec.y + leftRec.height &&
+      this.x - this.radius <= leftRec.x + leftRec.width &&
+      this.dx <= 0
+    ) {
       this.dx = -this.dx;
     }
-    if((this.y > rightRec.y && this.y < rightRec.y + rightRec.height) && (this.x + this.radius >= rightRec.x) && (this.dx >= 0)){
+    if (
+      this.y > rightRec.y &&
+      this.y < rightRec.y + rightRec.height &&
+      this.x + this.radius >= rightRec.x &&
+      this.dx >= 0
+    ) {
       this.dx = -this.dx;
     }
 
-    if(this.x - this.radius <= 0 || this.x + this.radius >= innerWidth){
-      initBall()
+    //when ball hits the edge of the screen horizontally
+    if (this.x - this.radius <= 0 || this.x + this.radius >= innerWidth) {
+      restart.style.display = "inline";
+      window.cancelAnimationFrame(myRequestId);
+      play = false;
+      ball = null;
+      if (this.x - this.radius <= 0) {
+        player2score += 1;
+        player2.textContent = `${player2score}`;
+      } else {
+        player1score += 1;
+        player1.textContent = `${player1score}`;
+      }
     }
 
     this.x += this.dx;
     this.y += this.dy;
-    this.draw();
+      this.draw();
   };
 }
 
 function init() {
   rectancles = [];
-  recLeft = new Rectangle(80-recWidth, posLeftY, recWidth, recHeight);
+  recLeft = new Rectangle(80 - recWidth, posLeftY, recWidth, recHeight);
   rectancles.push(recLeft);
   recRight = new Rectangle(innerWidth - 80, posRightY, recWidth, recHeight);
   rectancles.push(recRight);
@@ -120,7 +223,7 @@ function init() {
   rectancles[1].draw();
 }
 
-let speed = [Math.random() * 5 - 10, Math.random() * 5 + 5]
+let speed = [Math.random() * 5 - 10, Math.random() * 5 + 5];
 
 function initBall() {
   radius = 40;
@@ -132,15 +235,16 @@ function initBall() {
   ball.draw();
 }
 
-initBall();
-init();
+let myRequestId;
 
 function animate() {
-  requestAnimationFrame(animate);
-  c.clearRect(0, 0, innerWidth, innerHeight);
-  rectancles[0].update();
-  rectancles[1].update();
-  ball.update(rectancles[0], rectancles[1]);
+  if(pause){
+    return;
+  }
+    console.log("hi")
+    myRequestId = requestAnimationFrame(animate);
+    c.clearRect(0, 0, innerWidth, innerHeight);
+    rectancles[0].update();
+    rectancles[1].update();
+    ball.update(rectancles[0], rectancles[1]);
 }
-
-animate();
